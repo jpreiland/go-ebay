8f2c350959ae1c1aab7fcf4f0d61f4c2358a6864
John-Peter Reiland <jpreiland@gmail.com>
2013-06-02 21:05:21 -0400

    added price and location information to listing output

--- search-listings.go
+++ search-listings.go
@@ -5,14 +5,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"encoding/xml"
+	"strconv"
 )
 
 type Item struct {
 	ItemID string `xml:"itemId"`
 	Title string `xml:"title"`
+	Location string `xml:"location"`
+	CurrentPrice float64 `xml:"sellingStatus>currentPrice"`
+	ShippingPrice float64 `xml:"shippingInfo>shippingServiceCost"`
+	BINprice float64 `xml:"listingInfo>buyItNowPrice"`
 }
 
-type Response struct {
+type ResponseXML struct {
 	XMLName xml.Name `xml:"findItemsByKeywordsResponse"`
 	Items []Item `xml:"searchResult>item"`
 }
@@ -28,6 +33,8 @@ func buildRequest(appid, keywords, n string) string {
 	url += keywords
 	url += "&paginationInput.entriesPerPage="
 	url += n
+	//Comment out the next line if you do not want to limit results to Fixed Price and Auction with Buy It Now listings. 
+	url += "&itemFilter(0).name=ListingType&itemFilter(0).value(0)=FixedPrice&itemFilter(0).value(1)=AuctionWithBIN"
 
 	return url
 }
@@ -43,26 +50,45 @@ func keywordConvert(keywords string) string {
     return keywords
 }
 
+// print out information on each listing
+func printListings(v ResponseXML) {
+	for i := 0; i < len(v.Items); i++ {
+    	fmt.Println(strconv.Itoa((i+1)) + ". " + v.Items[i].Title)
+    	if(v.Items[i].BINprice != 0){
+    		fmt.Println("Buy It Now price: $" + strconv.FormatFloat(v.Items[i].BINprice, 'f', 2, 64))
+    	}else{
+    		fmt.Println("Current price: $" + strconv.FormatFloat(v.Items[i].CurrentPrice, 'f', 2, 64))
+    	}
+    	if(v.Items[i].ShippingPrice != 0){
+    		fmt.Println("Shipping: $" + strconv.FormatFloat(v.Items[i].ShippingPrice, 'f', 2, 64))
+    	}else{
+    		fmt.Println("Shipping: Free")
+    	}
+    	fmt.Println("Location: " + v.Items[i].Location)
+    	fmt.Println("-------------------------------------")
+    }
+}
 
-func main() {
-	// build request url
-	appID := "" //YOUR APPID HERE
-	query := "" //YOUR SEARCH QUERY HERE
-	num_items := "10" //NUMBER OF RESULTS TO RETURN
-	url := buildRequest(appID, query, num_items)
-
-	// send request and store response
+func sendAndProcessRequest(url string) ResponseXML {
 	r, _ := http.Get(url)
 	response, _ := ioutil.ReadAll(r.Body)
-	v := Response{}
+	v := ResponseXML{}
 	err := xml.Unmarshal([]byte(response), &v)
 	if err != nil {
    		fmt.Printf("error: %v", err)
-   		return
     }
+    return v
+}
 
-    // output information
-    for i := 0; i < len(v.Items); i++ {
-    	fmt.Println(v.Items[i].Title)
-    }
+
+func main() {
+	appID := "" // YOUR APPID HERE
+	query := "" // SEARCH QUERY HERE
+	num_items := "10"
+
+	url := buildRequest(appID, query, num_items)
+
+	v := sendAndProcessRequest(url)
+
+    printListings(v)    
 }
